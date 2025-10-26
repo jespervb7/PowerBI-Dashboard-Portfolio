4b2ee849bac7e92cef87440a46ba1d25ba6ac2a9
Jesper van Beemdelust <jespervanbeemdelust@hotmail.com>
2025-10-26 18:51:55 +0100

    test

--- ci_cd/bpa/bpa.ps1
+++ ci_cd/bpa/bpa.ps1
@@ -1,12 +1,13 @@
 param (
-    # Optional: allow passing in additional source paths, default to ./dashboards
-    $src = @("./dashboards")
+    $src = @("**\*.SemanticModel")        
 )
-
+  
 $currentFolder = (Split-Path $MyInvocation.MyCommand.Definition -Parent)
 
 if ($src.Length -eq 0) {
+    
     Write-Host "Please provide a valid path to the source files."
+
     return
 }
 
@@ -15,25 +16,35 @@ if ($src.Length -eq 0) {
 $toolsPath = "$currentFolder\_tools"
 
 $tools = @(
-    @{"tool" = "TabularEditor"; "downloadUrl" = "https://github.com/TabularEditor/TabularEditor/releases/latest/download/TabularEditor.Portable.zip"; "rulesUrl" = "https://raw.githubusercontent.com/microsoft/Analysis-Services/master/BestPracticeRules/BPARules.json" },
-    @{"tool" = "PBIInspector"; "downloadUrl" = "https://github.com/NatVanG/PBI-InspectorV2/releases/latest/download/win-x64-CLI.zip"; "rulesUrl" = "https://raw.githubusercontent.com/NatVanG/PBI-InspectorV2/refs/heads/main/Rules/Base-rules.json" }
-)
+    @{"tool" = "TabularEditor"; "downloadUrl" = "https://github.com/TabularEditor/TabularEditor/releases/latest/download/TabularEditor.Portable.zip"; "rulesUrl" = "https://raw.githubusercontent.com/microsoft/Analysis-Services/master/BestPracticeRules/BPARules.json" }
+   )
 
 foreach ($tool in $tools) {
+
     $toolName = $tool.tool
     $downloadUrl = $tool.downloadUrl
     $rulesUrl = $tool.rulesUrl
+
     $destinationPath = "$toolsPath\$toolName"
 
     if (!(Test-Path $destinationPath)) {
-        New-Item -ItemType Directory -Path $destinationPath -ErrorAction SilentlyContinue | Out-Null
+
+        New-Item -ItemType Directory -Path $destinationPath -ErrorAction SilentlyContinue | Out-Null            
+
         Write-Host "Downloading $toolName"
+
         $zipFile = "$destinationPath\$toolName.zip"
+
         Invoke-WebRequest -Uri $downloadUrl -OutFile $zipFile
-        Expand-Archive -Path $zipFile -DestinationPath $destinationPath -Force
-        Remove-Item $zipFile
+
+        Expand-Archive -Path $zipFile -DestinationPath $destinationPath -Force     
+
+        Remove-Item $zipFile        
+
+        # Downloading default rules
+
         Invoke-WebRequest -Uri $rulesUrl -OutFile "$destinationPath\defaultRules.json"
-    }
+    }    
 }
 
 #endregion
@@ -42,48 +53,45 @@ foreach ($tool in $tools) {
 
 $tabularEditorEXE = "$toolsPath\TabularEditor\TabularEditor.exe"
 $tabularEditorRulesPath = "$currentFolder\bpa-rules-semanticmodel.json"
-$pbiInspectorEXE = "$toolsPath\PBIInspector\win-x64\CLI\PBIRInspectorCLI.exe"
-$pbiInspectorRulesPath = "$currentFolder\bpa-rules-report.json"
 
 if (!(Test-Path $tabularEditorRulesPath)) {
+
     Write-Host "Using default rules for Tabular Editor"
     $tabularEditorRulesPath = "$toolsPath\TabularEditor\defaultRules.json"
 }
 
-if (!(Test-Path $pbiInspectorRulesPath)) {
-    Write-Host "Using default rules for PBI Inspector"
-    $pbiInspectorRulesPath = "$toolsPath\PBIInspector\defaultRules.json"
-}
-
 foreach ($srcPath in $src) {
-
+    
     if (!(Test-Path $srcPath)) {
         Write-Host "Source path '$srcPath' does not exist."
         continue
     }
 
-    # Recursively find all semantic model files (.pbidataset, .pbism)
-    $itemsFolders = Get-ChildItem -Path $srcPath -Recurse -Include "*.pbidataset", "*.pbism" | Sort-Object Name -Descending
+    $itemsFolders = Get-ChildItem  -Path $srcPath -recurse -include ("*.pbidataset", "*.pbism", "*.pbir") | Sort-Object Name -Descending
 
     foreach ($itemFile in $itemsFolders) {
+        
         # Semantic model
-        $itemPath = "$($itemFile.Directory.FullName)\definition"
+        if ($itemFile.Extension -in @(".pbidataset", ".pbism")) {
 
-        if (!(Test-Path $itemPath)) {
-            $itemPath = "$($itemFile.Directory.FullName)\model.bim"
+            $itemPath = "$($itemFile.Directory.FullName)\definition"
 
             if (!(Test-Path $itemPath)) {
-                throw "Cannot find semantic model definition for '$($itemFile.FullName)'."
+                $itemPath = "$($itemFile.Directory.FullName)\model.bim"
+
+                if (!(Test-Path $itemPath)) {
+                    throw "Cannot find semantic model definition."
+                }
             }
-        }
 
-        Write-Host "Running Tabular Editor BPA rules for: '$itemPath'"
-        $process = Start-Process -FilePath $tabularEditorEXE -ArgumentList """$itemPath"" -A ""$tabularEditorRulesPath"" -G" -NoNewWindow -Wait -PassThru
+            Write-Host "Running Tabular Editor BPA rules for: '$itemPath'"
 
-        if ($process.ExitCode -ne 0) {
-            throw "Error running rules for: '$itemPath'"
+            $process = Start-Process -FilePath $tabularEditorEXE -ArgumentList """$itemPath"" -A ""$tabularEditorRulesPath"" -G" -NoNewWindow -Wait -PassThru    
+
+            if ($process.ExitCode -ne 0) {
+                throw "Error running rules for: '$itemPath'"
+            }                
         }
     }
 }
-
 #endregion
